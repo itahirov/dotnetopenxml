d20ae227d1f7be6cb7ca538fc76506e5bfced4ca
Ismayil Tahirov (Collabera) <v-istahi@microsoft.com>
2021-10-07 21:02:35 -0400

    initialcommit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using X15 = DocumentFormat.OpenXml.Office2013.Excel;
+
+namespace WebApplication2.Controllers
+{
+
+    public class  HomeController : Controller
+    {
+        public static void CreateSpreadsheetWorkbook(string filepath)
+        {
+            // Create a spreadsheet document by supplying the filepath.
+            // By default, AutoSave = true, Editable = true, and Type = xlsx.
+            DocumentFormat.OpenXml.Packaging.SpreadsheetDocument spreadsheetDocument = DocumentFormat.OpenXml.Packaging.SpreadsheetDocument.
+                Create(filepath, SpreadsheetDocumentType.Workbook);
+
+            // Add a WorkbookPart to the document.
+            WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
+            workbookpart.Workbook = new Workbook();
+
+            // Add a WorksheetPart to the WorkbookPart.
+            WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
+            worksheetPart.Worksheet = new Worksheet(new SheetData());
+
+            // Add Sheets to the Workbook.
+            Sheets sheets = spreadsheetDocument.WorkbookPart.Workbook.
+                AppendChild<Sheets>(new Sheets());
+
+            // Append a new worksheet and associate it with the workbook.
+            Sheet sheet = new Sheet()
+            {
+                Id = spreadsheetDocument.WorkbookPart.
+                GetIdOfPart(worksheetPart),
+                SheetId = 1,
+                Name = "mySheet"
+            };
+            sheets.Append(sheet);
+
+            workbookpart.Workbook.Save();
+
+            // Close the document.
+            spreadsheetDocument.Close();
+        }
+
+        public static void InsertWorksheet(string docName)
+        {
+            // Open the document for editing.
+            using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(docName, true))
+            {
+                // Add a blank WorksheetPart.
+                WorksheetPart newWorksheetPart = spreadSheet.WorkbookPart.AddNewPart<WorksheetPart>();
+                newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+
+                Sheets sheets = spreadSheet.WorkbookPart.Workbook.GetFirstChild<Sheets>();
+                string relationshipId = spreadSheet.WorkbookPart.GetIdOfPart(newWorksheetPart);
+
+                // Get a unique ID for the new worksheet.
+                uint sheetId = 1;
+                if (sheets.Elements<Sheet>().Count() > 0)
+                {
+                    sheetId = sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+                }
+
+                // Give the new worksheet a name.
+                string sheetName = "Sheet" + sheetId;
+
+                // Append the new worksheet and associate it with the workbook.
+                Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+                sheets.Append(sheet);
+            }
+        }
+        public ActionResult Index()
+        {
+            //CreateSpreadsheetWorkbook(@"c:\Users\v-istahi\Documents\Sheet3.xlsx");
+            //CreateSpreadsheetWorkbook(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+"\\Sheet3.xlsx");
+            CreateSpreadsheetWorkbook(@"D:\Home\Sheet3.xlsx");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = Environment.GetEnvironmentVariable("HOMEDRIVE");
+            ViewBag.Message = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            ViewBag.Message = "D:\\home";
+            InsertWorksheet(@"D:\Home\Sheet3.xlsx");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
